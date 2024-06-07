8d9802b349c81c09a9740c948bfe6da9702f5f78
esmail sarwari <esmail.sarwari10@gmail.com>
2024-06-07 13:18:20 -0700

    partialization on static files done, static files middleware added, routes tested

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-const express = require('express');
-const app  = express();
+const express = require("express");
+// const ejs = require("ejs");
+const app = express();
 const port = 3000;
 
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    res.status(200).send('Hello express js');
-})
-
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/add_post", (req, res) => {
+  res.render("add_post");
+});
+app.get("/post", (req, res) => {
+  res.render("post");
+});
 
 app.listen(port, () => {
-    console.log(`server start running on port: ${port}`);
-})
+  console.log(`server start running on port: ${port}`);
+});
